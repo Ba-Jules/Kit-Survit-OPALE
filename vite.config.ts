db5ec051ee9c2e4ac323106271c6d7c44843ebad
Ba-Jules <ba_jules@live.fr>
2025-04-03 01:41:58 +0200

    Ajout de la configuration Vite pour GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Kit-Survit-OPALE/', // ✅ essentiel pour GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
